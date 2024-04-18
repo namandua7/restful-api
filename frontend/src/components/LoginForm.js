a5a986e37fcf558fcdce5472bccd0c9357bb6a7a
Naman Dua <duanaman9@gmail.com>
2024-04-18 16:59:31 +0530

    Implement axios for logging through react

--- frontend/src/components/LoginForm.js
+++ frontend/src/components/LoginForm.js
@@ -1,23 +1,43 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 export default function LoginForm(props) {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post(
+        'http://localhost:3000/api/v1/users/sign_in',
+        { email, password }
+      );
+      console.log(response.data);
+      navigate('/');
+    } catch (error) {
+      console.error('Error submitting form:', error);
+    }
+  };
+
   return (
     <div className="my-5 container">
-    <form className={`text-${props.mode==='dark'?'light':'dark'}`}>
-      <div className="mb-3">
-        <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
-        <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
-      </div>
-      <div className="mb-3">
-        <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
-        <input type="password" className="form-control" id="exampleInputPassword1"/>
-      </div>
-      <div className="mb-3 form-check">
-        <input type="checkbox" className="form-check-input" id="exampleCheck1"/>
-        <label className="form-check-label" htmlFor="exampleCheck1">Remember</label>
-      </div>
-      <button type="submit" className="btn btn-primary">Submit</button>
-    </form>
+      <form onSubmit={handleSubmit} className={`text-${props.mode === 'dark' ? 'light' : 'dark'}`}>
+        <div className="mb-3">
+          <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
+          <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" value={email}onChange={(e) => setEmail(e.target.value)}/>
+        </div>
+        <div className="mb-3">
+          <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
+          <input type="password" className="form-control" id="exampleInputPassword1" value={password}onChange={(e) => setPassword(e.target.value)}/>
+        </div>
+        <div className="mb-3 form-check">
+          <input type="checkbox" className="form-check-input" id="exampleCheck1" />
+          <label className="form-check-label" htmlFor="exampleCheck1">Remember</label>
+        </div>
+        <button type="submit" className="btn btn-primary">Submit</button>
+      </form>
     </div>
-  )
+  );
 }
