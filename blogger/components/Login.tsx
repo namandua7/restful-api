1ef65747e18361eb9b92f37878d6f7b74a11868c
Naman Dua <duanaman9@gmail.com>
2024-05-20 14:54:09 +0530

    Implemented error showing on login

--- blogger/components/Login.tsx
+++ blogger/components/Login.tsx
@@ -1,18 +1,22 @@
 import React, { useState } from 'react'
-import {handleLogin} from '../helpers'
+import { handleLogin } from '../helpers'
 import { useRouter } from 'next/router';
 import Link from 'next/link';
 
 export default function Login() {
-
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
   const router = useRouter();
 
   const handleSubmit = async (e: any) => {
     e.preventDefault();
-    handleLogin(e, email, password);
-    router.push('/chat');
+    try {
+      await handleLogin(email, password);
+      router.push('/chat');
+    } catch (error: any) {
+      setErrorMessage(error.message);
+    }
   };
 
   return (
@@ -20,16 +24,17 @@ export default function Login() {
       <h1 className='text-center mt-3'>Log in</h1>
       <div className="container my-5 d-flex justify-content-center">
         <div>
-        <div className="mb-3">
-          <label htmlFor="exampleFormControlInput1" className="form-label">Email address</label>
-          <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="Email" onChange={(e) => setEmail(e.target.value)}/>
-        </div>
-        <div className="mb-3">
-          <label htmlFor="exampleFormControlInput1" className="form-label">Password</label>
-          <input type="password" className="form-control" id="exampleFormControlInput1" placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
-        </div>
-        <span>Not an account ? </span><Link href="/sign-up">SignUp</Link><br />
-        <button type="submit" className="btn btn-primary my-2" onClick={handleSubmit}>Log in</button>
+          {errorMessage && <div className="alert alert-danger" role="alert">{errorMessage}</div>}
+          <div className="mb-3">
+            <label htmlFor="exampleFormControlInput1" className="form-label">Email address</label>
+            <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="Email" onChange={(e) => setEmail(e.target.value)} />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="exampleFormControlInput1" className="form-label">Password</label>
+            <input type="password" className="form-control" id="exampleFormControlInput1" placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
+          </div>
+          <span>Not an account? </span><Link href="/sign-up">SignUp</Link><br />
+          <button type="submit" className="btn btn-primary my-2" onClick={handleSubmit}>Log in</button>
         </div>
       </div>
     </>
--- blogger/helpers.js
+++ blogger/helpers.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
-export const handleLogin = async (e, email, password) => {
-  e.preventDefault();
+export const handleLogin = async (email, password) => {
   try {
     const response = await axios.post(
       'http://localhost:3001/api/v1/users/sign_in',
@@ -9,7 +8,11 @@ export const handleLogin = async (e, email, password) => {
     );
     console.log(response.data);
   } catch (error) {
-    console.error('Error submitting form:', error);
+    if (error.response && error.response.data && error.response.data.error) {
+      throw new Error(error.response.data.error);
+    } else {
+      throw new Error('An unexpected error occurred. Please try again later.');
+    }
   }
 };
 
