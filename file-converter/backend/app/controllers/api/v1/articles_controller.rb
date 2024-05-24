0b81fddc4fd9fe5fd3fdf56eafb9679f60bf6887
Naman Dua <duanaman9@gmail.com>
2024-05-24 11:48:31 +0530

    Setup backend to integerate with frontend and add active storage for file uploading

---
+++ file-converter/backend/app/controllers/api/v1/articles_controller.rb
@@ -0,0 +1,18 @@
+class Api::V1::ArticlesController < ApplicationController
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      render json: @article, status: :created
+    else
+      render json: @article.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :body, :image)
+  end
+
+end
